38633852c6cede5b77a34d5253430b08dbef5a73
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-10-24 09:59:50 +0530

    finding the missing element from the array

--- Problem/Problem14.java
+++ Problem/Problem14.java
@@ -3,7 +3,16 @@ package Problem;
 public class Problem14 {
     public static int getMissing(int[] array){
         int sum = 0;
-        int n = array.length+1;
+
+        int n = array.length + 1;
+
+        //changing the value of n if array contains 0
+        for(int i : array){
+            if(i == 0){
+                n = array.length;
+                break;
+            }
+        }
         for(int i : array){
             sum += i;
         }
@@ -13,7 +22,7 @@ public class Problem14 {
         return actualSum - sum;
     }
     public static void main(String [] args){
-        int []array = {1, 2, 4, 6, 3, 7, 8};
+        int []array = {1,2,4,5,6};
         System.out.println(getMissing(array));
     }
 }
